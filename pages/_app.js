70f5f952687e8d3fa5ea69cef8a0c7d2ec9eb751
ŞİMAL AYBÜKE KONCA <436541@ogr.ktu.edu.tr>
2026-03-31 20:39:22 +0300

    Fix:favicon.ico 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'; 
 import Layout from '@/components/Layout'; 
 import '@/styles/globals.css'; 
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
 export default function App({ Component, pageProps }) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <title>KTÜ Uzay Kulübü</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     );
 }
